4ee599cb3976f53755ba91f7c541425e0e185a36
Cha Yang <chachaiyang3@gmail.com>
2019-07-21 21:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function updateObjectWithKeyAndValue(object, key, value){
   var obj = { prop: 1 }
   return Object.assign({}, Object, {[key]: value})
 }
-updateObjectWithKeyAndValue(obj, 'prop 2', '2');
+updateObjectWithKeyAndValue(obj, 'prop 2', 2);
