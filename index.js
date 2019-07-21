8d3abe77f9ff8cfab67cb538e1070c16ee5aa385
Cha Yang <chachaiyang3@gmail.com>
2019-07-21 22:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function deleteFromObjectByKey(object, key){
 deleteFromObjectByKey(recipes, obj);
 function destructivelyDeleteFromObjectByKey(object, key){
   var obj = { prop: 1 }
-  delete 
+  delete obj.
 }
